4af5e3d754e539188176147fd37dc63d9168d805
Ryan kenidy <kenidyryan@gmail.com>
2023-03-07 08:40:41 +0700

    reset password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -36,9 +40,9 @@ class AuthController extends Controller
 
             // 3. create token
             if (!$user || !Hash::check($request->password, $user->password)) {
-                throw ValidationException::withMessages([
-                    'username' => ['The provided credentials are incorrect.'],
-                ]);
+                throw ValidationException::withMessages(
+                    ['message' => 'The provided credentials are incorrect']
+                );
             }
 
             // 4. return a response
@@ -49,7 +53,7 @@ class AuthController extends Controller
     }
 
     /**
-     * register the user.
+     * register
      *
      * @return \Illuminate\Http\Response
      */
@@ -61,7 +65,7 @@ class AuthController extends Controller
                 'email' => 'required|email|unique:users',
                 'username' => 'required|unique:users',
                 'password' => 'required',
-                'security_question_id' => 'required',
+                'security_question_id' => 'required|uuid',
                 'security_question_answer' => 'required',
             ]);
 
@@ -82,5 +86,96 @@ class AuthController extends Controller
         } catch (\Exception $e) {
             return handleException($e);
         }
+    }
+
+    /**
+     * validate forget password
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function validateForgetPassword(Request $request)
+    {
+        try {
+            // 1. validate request like email, body
+            $request->validate([
+                'username' => 'required',
+                'email' => 'required|email',
+                'security_question_answer' => 'required',
+            ]);
+
+            $authenticated = User::where('username', $request->username)
+                ->where('email', $request->email)
+                ->where('security_question_answer', $request->security_question_answer)
+                ->first();
+
+            if (!$authenticated) $this->sendValidationMessage();
+
+
+            $passwordResets =  $this->getPasswordReset($request);
+            if ($passwordResets) DB::table('password_resets')->where('email', $request->email)->delete();
+
+            //Create Password Reset Token
+            DB::table('password_resets')->insert([
+                'email' => $request->email,
+                'token' => Str::random(60),
+                'created_at' => Carbon::now()
+            ]);
+
+            //Get the token just created above
+            $tokenData = $this->getPasswordReset($request);
+
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'authenticated',
+                'token' => $tokenData->token
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return handleException($e);
+        }
+    }
+
+    /**
+     * reset password
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function resetPassword(Request $request)
+    {
+        try {
+            // 1. validate request like email, body
+            $request->validate([
+                'password' => 'required',
+                'reset_password_token' => 'required'
+            ]);
+
+            $tokenData = DB::table('password_resets')->where('token', $request->reset_password_token)->first();
+            if (!$tokenData) $this->sendValidationMessage();
+
+            $user = Auth::user();
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'success'
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return handleException($e);
+        }
+    }
+
+
+    private function getPasswordReset(Request $request)
+    {
+        return DB::table('password_resets')
+            ->where('email', $request->email)->first() ?? null;
+    }
+
+    private function sendValidationMessage()
+    {
+        throw ValidationException::withMessages(
+            ['message' => 'The provided credentials are incorrect']
+        );
     }
 }
